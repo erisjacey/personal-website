5e90e608c85d2846aed2542c511309ea90f68434
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-26 20:01:36 +0800

    Change page header styling

--- src/components/pageHeader/pageHeader.js
+++ src/components/pageHeader/pageHeader.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import './pageHeader.scss';
 
 const PageHeader = ({ name, subHeader }) => (
-  <div>
-    <h1>{name}</h1>
-    <h2>{subHeader}</h2>
+  <div className="page-header">
+    <h1 className="page-header__title">{name}</h1>
+    <h3 className="page-header__sub-header">{subHeader}</h3>
+    <div className="page-header__line" />
   </div>
 );
 
