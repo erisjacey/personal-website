1d2c1c6c0cd0d969e5ebee681487b33b1b96a1f6
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-27 10:30:00 +0800

    Rename Document component

--- src/components/document/document.js
+++ src/components/document/document.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './document.scss';
 
-const ProjectCard = ({ name }) => (
+const Document = ({ name }) => (
   <h5>{`Placeholder for ${name}`}</h5>
 );
 
-ProjectCard.propTypes = {
+Document.propTypes = {
   name: PropTypes.string.isRequired,
 };
 
-export default ProjectCard;
+export default Document;
