b904bc4d94b9e47c6dc01a1b7d2d723ae2310929
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-08-07 09:55:40 +0800

    Update document.js

--- src/components/document/document.js
+++ src/components/document/document.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './document.scss';
 
 const Document = ({ name }) => (
-  <h5>{`Placeholder for ${name}`}</h5>
+  <h5>Coming soon!</h5>
 );
 
 Document.propTypes = {
