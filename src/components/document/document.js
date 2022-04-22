03c1bdc9b2e5325353d54be76bb47ff53ead2db1
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2022-04-22 22:41:40 +0800

    Update Document

--- src/components/document/document.js
+++ src/components/document/document.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Grid,
+} from '@material-ui/core';
 import './document.scss';
 
-const Document = ({ name }) => (
-  <div style={{
-    height: '270px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }}
+const Document = ({ name, body }) => (
+  <Grid
+    container
+    direction="column"
+    justifyContent="center"
+    alignItems="center"
+    spacing={3}
   >
-    <h2>Coming soon!</h2>
-  </div>
+    {body.map((Component) => (
+      <Grid item>
+        <Component />
+      </Grid>
+    ))}
+  </Grid>
 );
 
 Document.propTypes = {
   name: PropTypes.string.isRequired,
+  body: PropTypes.arrayOf(PropTypes.func),
+};
+
+Document.defaultProps = {
+  body: [
+    () => (
+      <h2 style={{
+        height: '400px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      >
+        Coming soon!
+      </h2>
+    ),
+  ],
 };
 
 export default Document;
